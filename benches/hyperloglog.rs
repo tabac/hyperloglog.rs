6a328d4c829b9b18be31eff9716c1de5a95d5f2d
Tasos Bakogiannis <t.bakogiannis@gmail.com>
2020-07-24 23:13:05 +0300

    add benchmarks based on hyperloglog++ threshold.

--- benches/hyperloglog.rs
+++ benches/hyperloglog.rs
@@ -39,20 +39,20 @@ fn bench_add(c: &mut Criterion) {
         };
     }
 
-    bench_impls!["hyperloglog_add_p4", HyperLogLogPF, 4];
     bench_impls!["hyperloglog_add_p8", HyperLogLogPF, 8];
+    bench_impls!["hyperloglog_add_p14", HyperLogLogPF, 14];
     bench_impls!["hyperloglog_add_p16", HyperLogLogPF, 16];
 
-    bench_impls!["hyperloglogplus_add_p4", HyperLogLogPlus, 4];
     bench_impls!["hyperloglogplus_add_p8", HyperLogLogPlus, 8];
+    bench_impls!["hyperloglogplus_add_p14", HyperLogLogPlus, 14];
     bench_impls!["hyperloglogplus_add_p16", HyperLogLogPlus, 16];
 }
 
 fn bench_count(c: &mut Criterion) {
-    let workload = generate_strings(49200);
-
     macro_rules! bench_impls {
-        ($testname:expr, $impl:ident, $precision:expr) => {
+        ($testname:expr, $impl:ident, $precision:expr, $count:expr) => {
+            let workload = generate_strings($count);
+
             let mut hll: $impl<String, RandomState> =
                 $impl::new($precision, RandomState::new()).unwrap();
 
@@ -69,13 +69,48 @@ fn bench_count(c: &mut Criterion) {
         };
     }
 
-    bench_impls!["hyperloglog_count_p4", HyperLogLogPF, 4];
-    bench_impls!["hyperloglog_count_p8", HyperLogLogPF, 8];
-    bench_impls!["hyperloglog_count_p16", HyperLogLogPF, 16];
+    bench_impls!["hyperloglog_count_p8", HyperLogLogPF, 8, 20_000];
+    bench_impls!["hyperloglog_count_p14", HyperLogLogPF, 14, 100_000];
+    bench_impls!["hyperloglog_count_p16", HyperLogLogPF, 16, 5_000_000];
 
-    bench_impls!["hyperloglogplus_count_p4", HyperLogLogPlus, 4];
-    bench_impls!["hyperloglogplus_count_p8", HyperLogLogPlus, 8];
-    bench_impls!["hyperloglogplus_count_p16", HyperLogLogPlus, 16];
+    bench_impls![
+        "hyperloglogplus_count_p8_below_thres",
+        HyperLogLogPlus,
+        8,
+        200
+    ];
+    bench_impls![
+        "hyperloglogplus_count_p8_above_thres",
+        HyperLogLogPlus,
+        8,
+        20_000
+    ];
+
+    bench_impls![
+        "hyperloglogplus_count_p14_below_thres",
+        HyperLogLogPlus,
+        14,
+        10_000
+    ];
+    bench_impls![
+        "hyperloglogplus_count_p14_above_thres",
+        HyperLogLogPlus,
+        14,
+        100_000
+    ];
+
+    bench_impls![
+        "hyperloglogplus_count_p16_below_thres",
+        HyperLogLogPlus,
+        16,
+        50_000
+    ];
+    bench_impls![
+        "hyperloglogplus_count_p16_above_thres",
+        HyperLogLogPlus,
+        16,
+        5_000_000
+    ];
 }
 
 criterion_group!(benches, bench_add, bench_count);
